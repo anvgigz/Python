7807a1f80de2fcdbc286bb1a8ddf223e3ffe5598
Stephen <145696963+anvgigz@users.noreply.github.com>
2024-09-16 14:53:59 -0500

    Add files via upload
    first file uploads fro points

---
+++ knock-knock.py
@@ -0,0 +1,12 @@
+#this is a comment for points
+
+print("knocK-KnocK")
+print("Who's there?\n..........")
+
+print("chicken")
+print("Chicken who?\n......")
+print("Chicken your pockets, I want your money!")
+
+'''This can also be used to comment out code'''
+
+"""So can this"""
