a38abafa293ddd61e9a1a2251e72ac148f2cfbc3
Stephen <145696963+anvgigz@users.noreply.github.com>
2024-09-16 15:00:26 -0500

    Add files via upload
    PE1 2.2: Understanding Data Types in Python

---
+++ Data-types.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 19
+print("Literal:", x)
+
+# Integer
+y = -27
+print("Integer:", y)
+
+# Float
+z = 14.5
+print("Float:", z)
+
+# String
+name = "John is not my name"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
