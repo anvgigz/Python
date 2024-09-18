c0cd6f61b74bec27fb39e531376aca9ae5e37484
Stephen <145696963+anvgigz@users.noreply.github.com>
2024-09-18 09:11:07 -0500

    Add files via upload

---
+++ PE1 3.1 Evaluating Expressions and Making Decisions in Python.py
@@ -0,0 +1,21 @@
+print("Hello can you please tell me your age so I can determine if you can or cannot drive")
+age = int(input("Please enter your age: "))
+if age >= 16:
+    print("You are old enough to drive")
+else:
+    print("You are not old enough to drive")
+
+if age >= 18:
+    print("You are old enough to vote")
+else:
+    print("You are not old enough to vote")
+if  age >= 21:
+    print("You are old enough to drink")
+else:
+    print("You are not old enough to drink")
+
+if age >= 65:
+    print(" You are old enough to get a senior discount")
+else:
+    print("You are not old enough to get a senior discount")
+
