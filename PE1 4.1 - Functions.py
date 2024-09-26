c6947e30e6410552fc9f6056d7d0590e58490cb7
Stephen <145696963+anvgigz@users.noreply.github.com>
2024-09-26 15:20:15 -0500

    Update PE1 4.1 - Functions.py

--- PE1 4.1 - Functions.py
+++ PE1 4.1 - Functions.py
@@ -2,15 +2,15 @@
 
 #Square function
 
-def area_of_square(a,b): #calling the function with two parameters to be used
+def area_of_square(a,b): #calling the function with two parameters needing to be used
     area = a*b
     print(f"The area of the square is {area}")
 
-area_of_square(5,5)
+area_of_square(5,5)#Function called and executed
 
 #area of circle
 def area_of_circle(r):
     area = 3.14 * r**2
     print(f"The area of the circle is {area}")
 
-area_of_circle(5)
+area_of_circle(5)#Function called and executed
