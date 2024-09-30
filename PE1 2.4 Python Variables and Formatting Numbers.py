fe17930856503db0b09de3c29530dc8163d9579d
Stephen <145696963+anvgigz@users.noreply.github.com>
2024-09-30 14:43:29 -0500

    Update PE1 2.4 Python Variables and Formatting Numbers.py

--- PE1 2.4 Python Variables and Formatting Numbers.py
+++ PE1 2.4 Python Variables and Formatting Numbers.py
@@ -6,3 +6,15 @@ Pounds = Kilograms * 2.20462
 
 print(f"""when I convert the weight from Kilograms {Kilograms} to Pounds,
 I get {Pounds} Pounds""")
+
+Kilograms = 15.5
+
+print(f"when I convert the weight from Kilograms {Kilograms} to Pounds, I get {Pounds} Pounds")
+
+Kilograms = 50
+
+print(f"when I convert the weight from Kilograms {Kilograms} to Pounds, I get {Pounds} Pounds")
+
+Kilograms = 0.5
+
+print(f"when I convert the weight from Kilograms {Kilograms} to Pounds, I get {Pounds} Pounds")
