b2ba5bbd286f69cdf048d1af6f2eea6c0e6eab1b
Stephen <145696963+anvgigz@users.noreply.github.com>
2024-09-30 14:37:37 -0500

    Update usingcomments.py

--- usingcomments.py
+++ usingcomments.py
@@ -1,6 +1,7 @@
-
-
-
+# the tank_size represents the size in gallons of the gas tank
+# the distance represents the distance in miles that the car can travel
+# the mpg represents the miles per gallon that the car can travel
+# The print function displays the miles per gallon that the car can travel
 
 
 tank_size = 15
