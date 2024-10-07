c2009f2a47f6b380a890032ddfdc0b24c2999793
Stephen <145696963+anvgigz@users.noreply.github.com>
2024-10-07 08:44:30 -0500

    Update PE2 Module 1.1 and 1.2 - Modules.py

--- PE2 Module 1.1 and 1.2 - Modules.py
+++ PE2 Module 1.1 and 1.2 - Modules.py
@@ -9,13 +9,19 @@ def guessing_game():
     while answer is False:
         try:
             guess = int(input())
+            difference = abs(number_generated - guess) # finds the difference between the generated number and the guessed number
             if guess == number_generated:
-                print("You got it right")       # answer correctly guessed // answer is turned to True to break the loop.
+                print("Congratulations you guessed right")
                 answer = True
-            elif guess > number_generated:      # lets the user know their number is too high // function will call itself again
-                print("Your guess is too high")
-            elif guess < number_generated:  # lets the user know their number is too low // function will call itself again
-                print("Your guess is too low")
+            elif difference <= 5:   # if value guessed is less then or equal to the difference this wil be printed // stating how far away you are from the guess
+                print(f"Very hot. Difference: {difference}")
+            elif difference <= 15:  # if value guessed is less then or equal to the difference this wil be printed // stating how far away you are from the guess
+                print(f"Hot. Difference: {difference}")
+            elif difference <= 25:  # if value guessed is less then or equal to the difference this wil be printed // stating how far away you are from the guess
+                print(f"Cool. Difference: {difference}")
+            else:   # rather than doing a greater than sign for 25 an else statement will suffice and number of fdifference will be printed.
+                print(f"Cold. Difference: {difference}")
+           
         except ValueError:      # if the user enters a string instead of a number. function will call itself again
             print("Please enter a number you did not do that")
             continue
