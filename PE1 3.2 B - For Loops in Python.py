eb902f1d0fd179143b8705289f3b60c013bfc930
Stephen <145696963+anvgigz@users.noreply.github.com>
2024-10-17 15:09:50 -0500

    Update PE1 3.2 B - For Loops in Python.py

--- PE1 3.2 B - For Loops in Python.py
+++ PE1 3.2 B - For Loops in Python.py
@@ -1,7 +1,7 @@
 def divisible_by_7():
     list = [] # empty list to beused for appending numbers that are divisible by 7
-    for divisible in range(1,300): # numbers 1 - 299 will be used
-        if divisible % 7 == 0: # range(1,299) each number checked for perfect divisibility == 0 remainder
+    for divisible in range(1,301): # numbers 1 - 300 will be used
+        if divisible % 7 == 0: # range(1,300) each number checked for perfect divisibility == 0 remainder
             list.append(divisible) # numbers 0 remainder are added to the list
     print(list) #print new list
 
