5483208a2497146d73f4a8ef0ff0910fb7f4109d
Stephen <145696963+anvgigz@users.noreply.github.com>
2024-10-18 13:45:28 -0500

    Update PE1 3.4 Lists.py

--- PE1 3.4 Lists.py
+++ PE1 3.4 Lists.py
@@ -1,74 +1,20 @@
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-steps = [] # empty list ot append steps
+steps = []  # Empty list to append steps
 
-for day in days: # each day in list days will be executed in the while loop
-    while True:
-        try:
-            step_count = int(input(f"How many steps did you take on {day}? "))
-            steps.append(step_count)
-            break # used to move loop to next day
-        except ValueError:
-            print("Please enter a valid number.")
+for day in days:  # Each day in list days will be executed in the loop
+    try:
+        step_count = int(input(f"How many steps did you take on {day}? "))
+        steps.append(step_count)  # Append the step count to the list
+    except ValueError:
+        print("Please enter a valid number.")
 
-print("\nSteps recorded for each day:") # after the while loop completed the code continues here
-for day, step in zip(days, steps): # the zip function is used to pair the days with steps accordingly based on the order the they were input
-    print(f"{day}: {step} steps") #printeed ex - Monday: 2000 steps
+print("\nSteps recorded for each day:")  # After the loop is completed, the code continues here
+for day, step in zip(days, steps):  # The zip function is used to pair the days with steps accordingly
+    print(f"{day}: {step} steps")  # Printed ex - Monday: 2000 steps
 
-total_steps = sum(steps)  # the sum function adds all the steps together usign addition. and puts them into the variable total_steps
-print(f"\nTotal steps taken in the week: {total_steps}") #  # total_steps is printed in an f-string
+total_steps = sum(steps)  # The sum function adds all the steps together using addition and puts them into the variable total_steps
+print(f"\nTotal steps taken in the week: {total_steps}")  # Total steps is printed in an f-string
 
-average_steps = round(total_steps / len(steps)) # the total steps is divided by the amount of numbers in steps( which would be 7 based off the while loop of days of the week
+average_steps = round(total_steps / len(steps))  # The total steps is divided by the number of days in steps
 # This number is then rounded using the round function which provides an integer rather than a float
-print(f"Average steps taken per day: {average_steps}") # average per week is printed
-
-
-
-
-"""Got ahead of myself VVVVVVVVVvv"""
-
-# global_list = [1,2,3,4,5,6,7,8,9,10,11,12]
-
-# class lists():
-
- 
-#     def append_list():
-#         global global_list
-#         for number in range(13, 101):
-#             global_list.append(number)
-#         print(f"printing the list\n{global_list}\n")
-
-#     def removing_numbers():
-#         global global_list
-#         for number in global_list:
-#             if number % 2 ==0:
-#                 global_list.remove(number)
-#         print(f"printing the list\n{global_list}\n")
-
-#     def reverse_sort():
-#         global global_list
-#         global_list.reverse()
-#         print(f"printing the list\n{global_list}\n")
-
-#     def pop_list():
-#         global global_list
-#         global_list.pop()
-#         print(f"printing the list\n{global_list}\n")
-
-#     def len_list():
-#         global global_list
-#         print(f"printing the length of the list\n{len(global_list)}\n")
-
-#     def sort_list():
-#         global global_list
-#         global_list.sort()
-#         print(f"printing the list\n{global_list}\n")
-    
-
-
-# lists.append_list()
-# lists.removing_numbers()
-# lists.reverse_sort()
-# lists.pop_list()
-# lists.len_list()
-# lists.sort_list()
-
+print(f"Average steps taken per day: {average_steps}")  # Average per week is printed
